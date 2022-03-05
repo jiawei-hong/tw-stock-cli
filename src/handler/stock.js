fab7a02b01ec32e3bd258ae8cb02e009cf751408
jiawei-hong <zxc37984@gmail.com>
2022-03-05 18:01:57 +0800

    refactor:rewrite stock initialize handle

--- src/handler/stock.js
+++ src/handler/stock.js
@@ -12,102 +12,121 @@ class Stock {
     this.code = params.code
     this.options = params.options
     this.stockCategoryPath = path.resolve(`${__dirname}/../stock.json`)
-    this.p = new Table({
-      columns: [
-        { name: '代號', alignment: 'center' },
-        { name: '類別', alignment: 'center' },
-        { name: '公司', alignment: 'center' },
-        { name: '當盤成交價', color: 'yellow' },
-        { name: '當盤成交量' },
-        { name: '累積成交量' },
-        { name: '昨收', color: 'cyan' },
-        { name: '開盤' },
-        { name: '最高' },
-        { name: '最低' },
-        { name: '漲停', color: 'red' },
-        { name: '跌停', color: 'green' },
-        { name: '最近成交時刻', alignment: 'center' },
-      ],
-    })
+    this.field = [
+      { code: 'c', name: '代號', alignment: 'center' },
+      {
+        code: 'ex',
+        name: '類別',
+        alignment: 'center',
+        callback: (stock) => (stock.ex === 'tse' ? '上市' : '上櫃'),
+      },
+      { code: 'n', name: '公司', alignment: 'center' },
+      { code: 'z', name: '當盤成交價', color: 'yellow' },
+      { code: 'tv', name: '當盤成交量' },
+      { code: 'v', name: '累積成交量' },
+      { code: 'y', name: '昨收', color: 'cyan' },
+      { code: 'o', name: '開盤' },
+      { code: 'h', name: '最高' },
+      { code: 'l', name: '最低' },
+      { code: 'u', name: '漲停', color: 'red' },
+      { code: 'w', name: '跌停', color: 'green' },
+      { code: 't', name: '最近成交時刻', alignment: 'center' },
+      {
+        name: '漲跌幅',
+        callback: this.getStockUpsAndDownsPercentage,
+      },
+    ]
+    this.notExecIsNanHandle = ['c', 'ex', 'n', 't']
+    this.p = new Table({ columns: this.field })
   }
 
   initialize() {
-    if (this.options.multiple) {
-      if (this.checkStockFileExist()) {
-        this.stocks = JSON.parse(
-          fs.readFileSync(this.stockCategoryPath, 'utf-8')
+    if (this.options.multiple && !this.checkStockFileExist()) {
+      console.log(
+        Text.red(
+          'If you want search multiple stock, please run update command.'
         )
-
-        let stock = []
-
-        for (let stockCode of this.code.split('-')) {
-          stockCode = stockCode.toUpperCase()
-          let existTse = this.checkStockExistInCateogry(stockCode)
-          let existOtc = this.checkStockExistInCateogry(stockCode, 'otc')
-
-          if (existTse || existOtc) {
-            stock.push(`${existTse ? 'tse' : 'otc'}_${stockCode}.tw`)
-          } else {
-            console.log(Text.red(`Not found ${stockCode} stock.`))
-          }
-        }
-
-        if (stock.length > 0) {
-          this.url = `${this.prefix}${stock.join('|')}`
-        }
-      } else {
-        console.log(Text.red('Not found stock.json file.'))
-      }
-    } else {
-      this.url = `${this.prefix}${
-        this.options.listed
-      }_${this.code.toUpperCase()}.tw`
+      )
     }
 
-    if (this.url.length > 0) {
+    this.url = this.getStockUrl()
+
+    if (this.url) {
       axios.get(this.url).then((res) => {
         const data = res.data
+
         if (data.msgArray.length === 0) {
           console.log(Text.red('Not Found Stock.'))
         } else {
           data.msgArray.forEach((stock) => {
-            const upsAndDownsPercentage = Text.strIsNanHandle(
-              ((parseFloat(stock.z) - parseFloat(stock.y)) /
-                parseFloat(stock.y)) *
-                100
-            )
-            let stockData = {
-              代號: stock.c,
-              類別: stock.ex == 'tse' ? '上市' : '上櫃',
-              公司: stock.n,
-              昨收: Text.strIsNanHandle(stock.y),
-              開盤: Text.strIsNanHandle(stock.o),
-              最高: Text.strIsNanHandle(stock.h),
-              最低: Text.strIsNanHandle(stock.l),
-              漲停: Text.strConvertToDecimalPoint(stock.u),
-              跌停: Text.strConvertToDecimalPoint(stock.w),
-              當盤成交價: Text.strIsNanHandle(stock.z),
-              當盤成交量: Text.strIsNanHandle(stock.tv),
-              累積成交量: Text.strIsNanHandle(stock.v),
-              最近成交時刻: stock.t,
-              漲跌幅:
-                upsAndDownsPercentage.length > 1
-                  ? Text.percentageHandle(upsAndDownsPercentage)
-                  : upsAndDownsPercentage,
-            }
+            let stockField = {}
+
+            this.field.forEach((field) => {
+              stockField[field.name] = this.notExecIsNanHandle.includes(
+                field.code
+              )
+                ? stock[field.code]
+                : Text.strIsNanHandle(stock[field.code])
+
+              if (field.callback) {
+                stockField[field.name] = field.callback(stock)
+              }
+            })
 
             if (stock.ex == 'otc') {
-              delete stockData.漲停
-              delete stockData.跌停
+              delete stockField.漲停
+              delete stockField.跌停
             }
 
-            this.p.addRow(stockData)
+            this.p.addRow(stockField)
           })
 
           this.p.printTable()
         }
       })
     }
+  }
+
+  getStockUrl() {
+    if (this.options.multiple) {
+      this.stocks = this.getAllStockCategory()
+
+      let stock = []
+
+      for (let stockCode of this.code.split('-')) {
+        stockCode = stockCode.toUpperCase()
+        let existTse = this.checkStockExistInCateogry(stockCode)
+        let existOtc = this.checkStockExistInCateogry(stockCode, 'otc')
+
+        if (existTse || existOtc) {
+          stock.push(`${existTse ? 'tse' : 'otc'}_${stockCode}.tw`)
+        } else {
+          console.log(Text.red(`Not found ${stockCode} stock.`))
+        }
+      }
+
+      if (stock.length > 0) {
+        return `${this.prefix}${stock.join('|')}`
+      }
+    }
+
+    return `${this.prefix}${this.options.listed}_${this.code.toUpperCase()}.tw`
+  }
+
+  getStockUpsAndDownsPercentage(stock) {
+    let [yesterdayPrice, currentPrice] = [stock.y, stock.z]
+
+    const percenetage = Text.strIsNanHandle(
+      ((parseFloat(currentPrice) - parseFloat(yesterdayPrice)) /
+        yesterdayPrice) *
+        100
+    )
+
+    return isNaN(percenetage) ? '-' : Text.percentageHandle(percenetage)
+  }
+
+  getAllStockCategory() {
+    return JSON.parse(fs.readFileSync(this.stockCategoryPath), 'utf8')
   }
 
   checkStockExistInCateogry(stockCode, category = 'tse') {
