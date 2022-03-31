b89242a7355d02f5e58848460e524b8a2c84ff11
jiawei-hong <zxc37984@gmail.com>
2022-03-31 09:54:46 +0800

    1.2.1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Crawler = require('./crawler')
 const Favorite = require('./handler/favorite')
 
 function run() {
-  program.name('tw-stock').version('1.2.0')
+  program.name('tw-stock').version('1.2.1')
 
   program
     .command('stock')
