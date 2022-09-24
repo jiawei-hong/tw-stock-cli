30ce051d806ba9a253d578aed61d6d662035b20d
jiawei-hong <zxc37984@gmail.com>
2022-09-25 02:38:36 +0800

    chore: replace moment with date-fns

--- src/lib/Chart.ts
+++ src/lib/Chart.ts
@@ -1,37 +1,35 @@
 import * as asciichart from 'asciichart'
-import * as moment from 'moment'
+import { isAfter, isBefore, set } from 'date-fns'
+
 import {
   CHART_NOT_FOUND,
   CHART_START_DATE_IS_AFTER_END_DATE,
 } from '../message/Chart'
 import { displayFailed } from './Text'
 
-function getHisWithDate(date: string): moment.Moment {
-  const [hour, minute, seconds] = [0, 2, 4].map((len) =>
-    parseInt(date.substr(len, 2))
-  )
+function getHisWithDate(date: string): Date {
+  const [hour, minute] = [0, 2].map((len) => parseInt(date.substr(len, 2)))
 
-  return moment()
-    .set({
-      hour,
-      minute,
-      seconds,
-    })
-    .utcOffset(8)
+  return set(new Date(), {
+    hours: hour,
+    minutes: minute + 480, // 480 = 8 hours
+    seconds: 0,
+    milliseconds: 0,
+  })
 }
 
 function filterDrawChartDataWithTwoTime(data: any, date: any) {
   const startDate = getHisWithDate(date[0])
   const endDate = getHisWithDate(date[1])
 
-  if (startDate.isAfter(endDate)) {
+  if (isAfter(startDate, endDate)) {
     return displayFailed(CHART_START_DATE_IS_AFTER_END_DATE)
   }
 
   return data.filter((d: any) => {
     if (Object.keys(d).includes('ts')) {
       const date = getHisWithDate(d.ts)
-      return startDate.isBefore(date) && endDate.isAfter(date)
+      return isBefore(startDate, date) && isAfter(endDate, date)
     }
     return false
   })
@@ -58,4 +56,4 @@ function draw(
   }
 }
 
-export { filterDrawChartDataWithTwoTime, draw }
+export { draw, filterDrawChartDataWithTwoTime }
