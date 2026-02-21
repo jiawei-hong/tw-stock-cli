275aad339247fc965300678c48c6db482ea9b451
Wei Hong <zxc37984@gmail.com>
2026-02-21 18:42:15 +0800

    feat: add shell tab-completion using omelette
    Add tab-completion support for commands, subcommands, options, and
    index codes. Users can enable it with `tw-stock completion` and
    remove it with `tw-stock completion --cleanup`.

---
+++ src/completion.ts
@@ -0,0 +1,12 @@
+import omelette from 'omelette'
+
+const completion = omelette('tw-stock <command>')
+
+completion.tree({
+  stock: ['--listed', '--multiple', '--favorite', '--oddLot', '--date', '--details'],
+  index: ['TAIEX', 'TWO', 'FRMSA', '--multiple', '--time', '--chart'],
+  crawler: [],
+  favorite: ['create', 'add', 'delete', 'list'],
+})
+
+export default completion
