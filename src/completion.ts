95069f92f5c10e3975fe958a0be53a82b310e747
Wei Hong <zxc37984@gmail.com>
2026-02-21 18:44:17 +0800

    style: format completion tree array

--- src/completion.ts
+++ src/completion.ts
@@ -3,7 +3,14 @@ import omelette from 'omelette'
 const completion = omelette('tw-stock <command>')
 
 completion.tree({
-  stock: ['--listed', '--multiple', '--favorite', '--oddLot', '--date', '--details'],
+  stock: [
+    '--listed',
+    '--multiple',
+    '--favorite',
+    '--oddLot',
+    '--date',
+    '--details',
+  ],
   index: ['TAIEX', 'TWO', 'FRMSA', '--multiple', '--time', '--chart'],
   crawler: [],
   favorite: ['create', 'add', 'delete', 'list'],
